e931975559707585c878e924bf3ce78881d218fb
Derek <dyin00@stuy.edu>
2018-11-01 11:46:58 -0400

    commt

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,1 +1,28 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+  public T set(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    else super.set(index, element);
+  }
+  public boolean add(T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    else super.add(element);
+  }
+  public void add(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    else super.add(index, element);
+  }
 
+}
